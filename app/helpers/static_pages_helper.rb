ac188ed5a0167ff19391d42cd47a6c8d03788442
lukaszub <lukaszub877@gmail.com>
2019-06-05 15:10:20 +0200

    Added parials for header and footer

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,16 @@
 module StaticPagesHelper
+	
+	def title_method(page_title = '')
+		title = "ListApp"
+
+		if page_title.empty?
+			title
+		else
+			page_title
+		end	
+	end
+
+	def footer_method
+		
+	end
 end
